87f9e3803b7f5c00fadc24569984126b9eb8f228
Darius Moseley <38228404+dmoseley@users.noreply.github.com>
2019-02-22 22:06:44 -0800

    Updated board tests (including sonar)

--- src/test/java/cs361/battleships/models/BoardTest.java
+++ src/test/java/cs361/battleships/models/BoardTest.java
@@ -2,8 +2,7 @@ package cs361.battleships.models;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.*;
 
 public class BoardTest {
 
@@ -11,39 +10,48 @@ public class BoardTest {
     public void testInvalidPlacement() {
         Board board = new Board();
         assertFalse(board.placeShip(new Ship("MINESWEEPER"), 11, 'C', true));
-        
+
     }
-    
+
     @Test
     public void testAttackBounds() {
         Board board = new Board();
         assertTrue(board.attack(15, 'C').getResult() == AtackStatus.INVALID);
     }
-    
+
     @Test
     public void testTwoShipsSameLoc() {
         Board board = new Board();
         assertTrue(board.placeShip(new Ship("MINESWEEPER"), 4, 'E', true));
         assertFalse(board.placeShip(new Ship("BATTLESHIP"), 4, 'E', true));
     }
-  
-	
-    @Test 
-    public void testCaptainQuarters() {
-      Board board = new Board();
-      assertTrue(board.placeShip(new Ship("MINESWEEPER"), 4, 'E', true));
-      assertTrue(board.attack(4, 'E').getResult() == AtackStatus.SUNK);
-    }
-	
-    
+
+
     @Test
-     public void testAttackSunk() 
-     {
+    public void testCaptainQuarters() {
+        Board board = new Board();
+        assertTrue(board.placeShip(new Ship("MINESWEEPER"), 4, 'E', true));
+        assertTrue(board.attack(4, 'E').getResult() == AtackStatus.SUNK);
+    }
+
+
+    @Test
+    public void testAttackSunk()
+    {
         Board board = new Board();
         board.placeShip(new Ship("MINESWEEPER"), 4, 'E', true);
         board.placeShip(new Ship("DESTROYER"), 1, 'A', true);
         board.attack(4, 'E');
         assertTrue(board.attack(5, 'E').getResult() == AtackStatus.SUNK);
+    }
+
+    @Test
+    public void testSonar() {
+
+        Game g = new Game();
+        g.placeShip(new Ship("MINESWEEPER"), 4,'E', true);
+        assertTrue(g.getOpponentsBoard().sonar(g, 10, 'E').size() == 0);
+
     }
 
 }
